c3fb3c4269e4b083800ae075e27e3e84dba6a211
Bernard Almeida da Costa <biernard@gmail.com>
2023-12-15 22:40:23 +0100

    created the upload property section

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Update from "./components/Update";
+import Upload from "./components/Upload";
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/update" element={<Update />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
     </>
   );
