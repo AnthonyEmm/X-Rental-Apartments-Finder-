1b2f9db87e8b13792a1724dd2d8c3cf7e9a91971
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2023-12-15 19:20:05 +0100

    created a Mongoose Middleware Pre FindOne lowercase query before execution

--- models/users.model.js
+++ models/users.model.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  email: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true, lowercase: true },
   password: { type: String, required: true, select: false },
   name: { type: String },
   avatar: { type: String },
+});
+
+userSchema.pre("findOne", function (next) {
+  const query = this.getQuery();
+  if (query.email) query.email = query.email.toLowerCase();
+  next();
 });
 
 module.exports = model("User", userSchema);
