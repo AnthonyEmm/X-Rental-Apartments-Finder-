84cc7f67775ab3908601f0519e40162ae7d3dd53
Bernard Almeida da Costa <biernard@gmail.com>
2024-01-03 10:34:31 +0100

    added new navbar

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -1,72 +1,51 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
 import "./NavBar.css";
-import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-inherit container-fluid px-4">
-      <Link to="/" className="navbar-brand fw-bold fs-4">
-        <span>X Rental</span>
-      </Link>
+    <nav className="navbar navbar-expand-lg bg-inherit">
+      <div className="container-fluid px-4">
+        <a className="navbar-brand fw-bold fs-4" href="#">
+          <span>X Rental</span>
+        </a>
 
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon custom-toggler-icon"></span>
+        </button>
 
-      <div
-        className="collapse navbar-collapse justify-content-end"
-        id="navbarNav"
-      >
-        <ul className="navbar-nav gap-3 ">
-          <li className="nav-item ">
-            <Link to="/" className="nav-link ">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item ">
-            <Link to="#" className="nav-link ">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/login" className="btn">
-              Sign-in
-            </Link>
-          </li>
-        </ul>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav gap-3">
+            <li className="nav-item align-self-end">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item align-self-end">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item align-self-end">
+              <a className="nav-link" href="#">
+                Sign-in
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
-
-  /* <>
-      <Navbar className="nav navbar-dark text-success border-bottom border-success sticky-top">
-        <Container>
-          <Navbar.Brand className="logo" href="#home">
-            X RENTAL
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#">About</Nav.Link>
-          </Nav>
-          <div className="links d-flex gap-3">
-            <Button variant="outline-success">Post Property</Button>
-            <Button variant="outline-success">Sign In</Button>
-          </div>
-        </Container>
-      </Navbar>
-    </> */
 }
 
 export default NavBar;
