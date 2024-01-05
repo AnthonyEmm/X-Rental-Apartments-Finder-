3938ea2c21086b0d48eb8269d770fdd33b4bdfe9
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-01-05 17:36:13 +0100

    refactored the propertydetails route

--- Client/src/components/PropertyDetails.jsx
+++ Client/src/components/PropertyDetails.jsx
@@ -23,73 +23,25 @@ function PropertyDetails() {
     <div className="bg text-white">
       <div className="container-main d-flex justify-content-center align-items-center d-flex mt-5">
         <div className="list-container border border-secondary rounded d-flex gap-5 p-5">
-          <div className="search-tags d-flex flex-column gap-2">
-            <div>
-              <div class="row">
-                <div class="col-md-5 d-flex flex-column gap-2">
-                  <img src="../ap.png" class="img-fluid" alt="Imagem 1" />
-                  <img src="../ap.png" class="img-fluid" alt="Imagem 2" />
-                  <img src="../ap.png" class="img-fluid" alt="Imagem 3" />
-                  <img src="../ap.png" class="img-fluid" alt="Imagem 4" />
-                </div>
-
-                <div class="col-md-7 d-flex flex-column gap-2">
-                  <img
-                    src="../ap.png"
-                    class="img-fluid custom-big-image"
-                    alt="Imagem 5 (maior)"
-                  />
-                  <div className="col-md-8 d-flex flex-column gap-2">
-                    <img src="../ap.png" class="img-fluid" alt="Imagem 6" />
-                    <img src="../ap.png" class="img-fluid" alt="Imagem 7" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
           {properties.map((property) => {
             return (
-              <div key={property.id}>
+              <div className="details" key={property.id}>
                 <p>
                   <img src={property.image} />
-                  {/* <img src={property.images[2]} /> */}
                 </p>
-
                 <h2>Title: {property.title}</h2>
-                {/* <h3>Owner: {property.owner}</h3> */}
                 <h3>Description: {property.description}</h3>
-                <h3>Property Area: {property.area}</h3>
+                <h3>Property Size: {property.area}</h3>
+                <h3>Rooms: {property.rooms}</h3>
+                <h3>Zip Code: {property.areaCode}</h3>
+                <h3>Type: {property.propertyType}</h3>
+                <h3>Year: {property.year}</h3>
                 <h3>Price: {property.price}</h3>
+                <h3>Availability: {property.availability}</h3>
+                {/* <h3>Owner: {property.owner}</h3> */}
               </div>
             );
           })}
-
-          <div className="list d-flex flex-column gap-3">
-            <div className="title-icon d-flex justify-content-between">
-              <h2>Flat in Pankow</h2>
-              <p className="mt-2">Icon</p>
-            </div>
-            <div className="text-price">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Corrupti nihil odit praesentium deserunt sed fuga ut molestiae
-                impedit eum suscipit debitis quasi iusto a velit magnam aut
-                dolor, dolores optio.
-              </p>
-              <p className="d-flex justify-content-end">1.2000 EUR/mo. warm</p>
-            </div>
-            <hr />
-            <div className="map-section d-flex flex-column gap-4 justify-content-center">
-              <img className="mapa" src="../map.png" alt="" />
-              <button className="btn btn-outline-success">
-                Contact the owner
-              </button>
-              <div className="tags d-flex justify-content-end">
-                <a href="#">Get back</a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
