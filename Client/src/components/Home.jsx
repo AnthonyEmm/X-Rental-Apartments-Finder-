ee364b914438ade2500f4f188b9eb8c497f3ea30
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-01-15 17:27:14 +0100

    fixed the page scrolling behavior

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
       loop: true,
       direction: "normal",
       begin: () => {
-        window.scrollTo(0, 0);
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
       },
     });
   }, []);
