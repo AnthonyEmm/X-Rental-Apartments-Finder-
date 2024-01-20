958a11117f544c9316d8fce103ed639de95b3ac0
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-01-20 06:03:08 +0100

    added the err function in uploads

--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -27,7 +27,12 @@ const cloudinaryUpload = async (req, res, next) => {
     for (const file of files) {
       const response = await cloudinary.uploader.upload(file.path);
       images.push(response.secure_url);
-      fs.unlink(file.path, (err, data) => {});
+      fs.unlink(file.path, (err, data) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+      });
     }
 
     req.images = images;
