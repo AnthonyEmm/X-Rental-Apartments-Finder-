30d80afab80577e774b6a13bb1c734f84f73d7a0
Bernard Almeida da Costa <biernard@gmail.com>
2024-01-27 00:18:39 +0100

    fixed the media query in the Property Details page

--- Client/src/components/PropertyDetails.jsx
+++ Client/src/components/PropertyDetails.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../components/PropertyDetails.css";
 import { Link, useParams } from "react-router-dom";
 import axiosClient from "../axiosClient";
@@ -23,57 +22,13 @@ function PropertyDetails() {
     }
   };
   const center = [52.5232, 13.3653];
-  const [name, setName] = useState("");
-  const [comment, setComment] = useState("");
-  const [submit, setSubmit] = useState([]);
-  const [error, setError] = useState(false);
-
-  const handleInputName = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleInputComment = (e) => {
-    setComment(e.target.value);
-  };
-
-  const handleSubmitClick = (e) => {
-    e.preventDefault();
-    if (name === "" || comment === "") {
-      setError(true);
-      setTimeout(() => {
-        setError(false);
-      }, 3000);
-    } else {
-      const submitOne = { id: new Date().getTime(), name, comment };
-      setSubmit([...submit, submitOne]);
-      setName("");
-      setComment("");
-    }
-  };
-
-  useEffect(() => {
-    const handleKeyPress = (e) => {
-      if (e.key === "Enter") {
-        handleSubmitClick(e);
-      }
-    };
-    document.addEventListener("keypress", handleKeyPress);
-    return () => {
-      document.removeEventListener("keypress", handleKeyPress);
-    };
-  }, [handleSubmitClick]);
-
-  const handleRemoveComment = (commentId) => {
-    const updatedSubmit = submit.filter((comment) => comment.id !== commentId);
-    setSubmit(updatedSubmit);
-  };
 
   return (
     <div className="bg text-white">
       {property && (
         <div className="container-main d-flex justify-content-center align-items-center d-flex mt-5">
-          <div className="list-container d-flex flex-column rounded gap-5 p-5 w-75 ">
-            <div className="left-side d-flex justify-content-center gap-5">
+          <div className="list-container d-flex flex-column gap-1 px-5">
+            <div className="left-side d-flex justify-content-center gap-5 px-5">
               <div className="img">
                 <ImageGallery
                   additionalClass=""
@@ -85,7 +40,7 @@ function PropertyDetails() {
                   })}
                 />
               </div>
-              <div className="details d-flex flex-column fw-bold">
+              <div className="details d-flex flex-column fw-bold px-2">
                 <h3>{property.description}</h3>
                 <p>
                   <span>Type</span>: {property.propertyType}
@@ -111,34 +66,32 @@ function PropertyDetails() {
                 <p>
                   <span>Owner</span>: {property.owner.name}
                 </p>
-                <div className="button-section mt-5 d-flex  justify-content-between">
+                <div className="button-section mt-5 d-flex justify-content-between px-2">
                   <Link to={`/profile/${property.owner._id}`}>
                     <button
-                      className="btn btn-lg btn-success rounded-1 "
+                      className="btn btn-success rounded-1 "
                       type="button"
                     >
                       OWNER DETAILS
                     </button>
                   </Link>
-                  <Link
-                    to="/listings"
-                    className="btn btn-lg btn-success rounded-1 text-black "
-                  >
-                    Go back
+                  <Link to="/listings">
+                    <button className="btn btn-success rounded-1" type="button">
+                      BACK
+                    </button>
                   </Link>
                 </div>
               </div>
             </div>
-            <div className="right-side d-flex align-items-center  gap-5">
-              <div className="map">
+            <div className="right-side d-flex justify-content-center align-items-center gap-5">
+              <div className="map" id="map">
                 <h3>View Location</h3>
                 <MapContainer
-                  className="custom-leaflet"
                   center={center}
-                  zoom={15}
+                  zoom={14}
                   style={{
-                    height: "50vh",
-                    width: "610px",
+                    height: window.innerHeight >= 768 ? "400px" : "30vh",
+                    width: window.innerWidth >= 768 ? "600px" : "80vw",
                   }}
                 >
                   <TileLayer
