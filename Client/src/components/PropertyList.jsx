b25d2d0b0ebdef8b5ba5cf0e843e0b3c0a1993bc
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-01-27 18:28:48 +0100

    fixed the width of the searchbar and filter size

--- Client/src/components/PropertyList.jsx
+++ Client/src/components/PropertyList.jsx
@@ -92,14 +92,14 @@ function PropertyList() {
                   searchParams.delete("page");
                   fetchProperties(
                     `/properties?${searchParams.toString()}`,
-                    true
+                    true,
                   );
                 }}
               >
                 Search
               </button>
             </div>
-            <div className="tags grid-container bg-transparent d-flex justify-content-center gap-4">
+            <div className="tags bg-transparent d-flex justify-content-center gap-4">
               <select
                 className="select"
                 onChange={(e) => {
